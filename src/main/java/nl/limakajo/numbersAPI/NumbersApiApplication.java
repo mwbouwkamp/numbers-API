842971bda3ac63e5fc121856e1c5e5610ef918e3
mwbouwkamp <mwbouwkamp@gmail.com>
2019-10-30 18:23:22 +0100

    Initial commit

---
+++ src/main/java/nl/limakajo/numbersAPI/NumbersApiApplication.java
@@ -0,0 +1,13 @@
+package nl.limakajo.numbersAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersApiApplication.class, args);
+	}
+
+}
