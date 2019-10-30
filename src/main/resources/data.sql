36769e97632c32a5e4cfa0465825a70bdb1506b3
mwbouwkamp <mwbouwkamp@gmail.com>
2019-10-30 19:03:32 +0100

    Setup h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS numbers_levels;
+
+CREATE TABLE numbers_levels (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numbers VARCHAR(250),
+    averagetime INT,
+    timesplayed INT,
+);
+
+INSERT INTO numbers_levels (numbers, averagetime, timesplayed) VALUES
+    ('001002003004005006007', 45000, 1),
+    ('002004006008010012014', 45000, 1),
+    ('003006009012015018021', 45000, 1);
